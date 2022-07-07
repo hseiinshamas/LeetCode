2a5506b5fadad8041daf3e66a6dd01580c41c306
Hussein Shamas <hssenshamas@gmail.com>
2022-07-07 15:48:09 +0300

    Time: 6 ms (83.85%), Space: 43.1 MB (84.19%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public int romanToInt(String s) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+
+		int result = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+
+			char c = s.charAt(i);
+
+			if (i > 0 && map.get(c) > map.get(s.charAt(i - 1))) {
+				result += map.get(c) - 2 * map.get(s.charAt(i - 1));
+			}
+
+			else {
+				result += map.get(c);
+			}
+
+		}
+
+		return result; 
+        
+    }
+}
