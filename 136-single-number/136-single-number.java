ff4698ff8bbb6de4d552bd009785426bb4a16d96
Hussein Shamas <hssenshamas@gmail.com>
2022-07-07 16:11:04 +0300

    Time: 2 ms (62.93%), Space: 50.8 MB (39.59%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+    
+        
+        int result = 0 ; 
+        
+        for(int i = 0 ; i<nums.length; i++){
+            result = result ^ nums[i]; 
+        }
+        return result ; 
+    
+    }
+
+} 
+
+
+
+    
+    
