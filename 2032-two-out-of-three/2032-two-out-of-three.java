00447687e783bb6d4488744a076007ce8c1c2467
Hussein Shamas <hssenshamas@gmail.com>
2022-07-10 17:40:16 +0300

    Time: 11 ms (35.55%), Space: 47.2 MB (44.38%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,54 +1,17 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         
-          List<Integer> list = new ArrayList<>();
-        
-        Set<Integer>mainSet=new HashSet<>();
-        Set<Integer>set1=new HashSet<>();
-        Set<Integer>set2=new HashSet<>();
-        Set<Integer>set3=new HashSet<>();
-        
-        for (var i:nums1
-             ) {
-            mainSet.add(i);
-        }
-        
-        for (var i:nums2
-             ) {
-            mainSet.add(i);
-        }
-        
-        for (var i:nums3
-             ) {
-            mainSet.add(i);
-        }
-
-        for (var i:nums1
-        ) {
-            set1.add(i);
-        }
-
-        for (var i:nums2
-        ) {
-            set2.add(i);
-        }
-
-        for (var i:nums3
-        ) {
-            set3.add(i);
-        }
-
-        for (var i:mainSet
-             ) {
-            if(set1.contains(i)&&set2.contains(i) || 
-                    set1.contains(i) && set3.contains(i) || set2.contains(i)&&set3.contains(i)){
-                list.add(i);
-            }
-            
-        }
-        
-        return list;
-
-        
+      Set<Integer> set = new HashSet();
+        Set<Integer> result =  new HashSet();
+        for(int i : nums1) //add all number on nums1 to set
+            set.add(i);
+        for(int i : nums2) // findout if there any intersection with nums2
+            if(set.contains(i)) result.add(i);
+        for(int i : nums2) //lately add all numbers from nums2 to set.
+            set.add(i);
+        for(int i : nums3) // check if there is intersection with again for nums3
+            if(set.contains(i)) result.add(i);
+        return new ArrayList(result);
+    
     }
 }
