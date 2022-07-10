b29c1c5d1ef432a3a4511233a27c42f6203f10c6
Hussein Shamas <hssenshamas@gmail.com>
2022-07-10 18:02:52 +0300

    Time: 1 ms (98.62%), Space: 41.4 MB (95.32%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,28 +1,19 @@
 public class Solution {
+
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-         int pos = Integer.SIZE - 1;     // maintains shift
- 
-        // store reversed bits of `n`. Initially, all bits are set to 0
         int reverse = 0;
- 
-        // do till all bits are processed
-        while (pos >= 0 && n != 0)
-        {
-            // if the current bit is 1, then set the corresponding bit in the result
-            if ((n & 1) != 0) {
-                reverse = reverse | (1 << pos);
+        int count = 0;
+
+        while (count++ < 32) {
+            reverse = reverse << 1;
+
+            if (n != 0) {
+                reverse = (n & 1) | reverse;
+
+                n = n >> 1;
             }
- 
-            n >>= 1;                    // drop current bit (divide by 2)
-            pos--;                      // decrement shift by 1
         }
- 
         return reverse;
-    }
- 
-    public static String toBinaryString(int n) {
-        return String.format("%32s", Integer.toBinaryString(n))
-                    .replaceAll(" ", "0");
     }
 }
