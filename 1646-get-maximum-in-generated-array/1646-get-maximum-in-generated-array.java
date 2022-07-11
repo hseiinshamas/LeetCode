01b4322430b14b485e45c0dc385b779d3e40f7ae
Hussein Shamas <hssenshamas@gmail.com>
2022-07-11 18:27:42 +0300

    Time: 1 ms (68.95%), Space: 41.5 MB (16.53%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -14,7 +14,9 @@ class Solution {
                 arr[i] = arr[a] + arr[b];
             }
         }
-        for (int i = 0; i < arr.length; i++) max = Math.max(max, arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) max = arr[i];
+        }
         return max;
     }
 }
