f0b9afcc50cb5389e7b01a345a060ed0c762d9cf
Hussein Shamas <hssenshamas@gmail.com>
2022-07-13 14:55:29 +0300

    Time: 52 ms (41.10%), Space: 44.6 MB (78.04%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,15 +1,19 @@
-import java.util.Arrays; 
+import java.util.Arrays;
+
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
-         int[] result = new int[2];
-        for (int i = 0; i < nums.length-1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target - nums[i] == nums[j]) {
-                    result[0] = i;
-                    result[1] = j;
+        int[] output = new int[2];
+        int i, j;
+
+        for (i = 0; i < nums.length; i++) {
+            for (j = i+1 ; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    output[0] = i;
+                    output[1] = j;
                 }
             }
         }
-        return result;
+        return output; 
     }
 }
