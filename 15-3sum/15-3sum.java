4e064676aabe9d02f5586cc1cbf4a309394cf9f9
Hussein Shamas <hssenshamas@gmail.com>
2022-07-16 10:47:33 +0300

    Time: 26 ms (86.16%), Space: 46.9 MB (86.78%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,38 +1,35 @@
 class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        // Sort the given array
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        List<List<Integer>> result = new ArrayList<>();
-        for (int num1Idx = 0; num1Idx + 2 < nums.length; num1Idx++) {
-            // Skip all duplicates from left
-            // num1Idx>0 ensures this check is made only from 2nd element onwards
-            if (num1Idx > 0 && nums[num1Idx] == nums[num1Idx - 1]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            int num2Idx = num1Idx + 1;
-            int num3Idx = nums.length - 1;
-            while (num2Idx < num3Idx) {
-                int sum = nums[num2Idx] + nums[num3Idx] + nums[num1Idx];
-                if (sum == 0) {
-                    // Add triplet to result
-                    result.add(Arrays.asList(nums[num1Idx], nums[num2Idx], nums[num3Idx]));
-
-                    num3Idx--;
-
-                    // Skip all duplicates from right
-                    while (num2Idx < num3Idx && nums[num3Idx] == nums[num3Idx + 1]) num3Idx--;
-                } else if (sum > 0) {
-                    // Decrement num3Idx to reduce sum value
-                    num3Idx--;
+            int toFind = -nums[i];
+            int low = i + 1, high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[low] + nums[high];
+                if (sum == toFind) {
+                    List<Integer> curr = new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[low]);
+                    curr.add(nums[high]);
+                    ans.add(curr);
+                    low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low - 1]) low++;
+                    while (low < high && nums[high] == nums[high + 1]) high--;
+                } else if (sum < toFind) {
+                    low++;
                 } else {
-                    // Increment num2Idx to increase sum value
-                    num2Idx++;
+                    high--;
                 }
             }
         }
-        return result;
+        return ans;
     }
 }
