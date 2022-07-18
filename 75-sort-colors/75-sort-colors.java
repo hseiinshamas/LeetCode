bfa0b15840187abaa96be866ac79bbe36fb2d907
Hussein Shamas <hssenshamas@gmail.com>
2022-07-18 13:08:22 +0300

    Time: 1 ms (41.42%), Space: 41.1 MB (84.42%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,52 +1,16 @@
-class Solution {
+class Solution { // merge sort
 
     public void sortColors(int[] nums) {
-        int[] output = new int[nums.length];
-        int len = nums.length;
-        if (len < 2) return;
+        for (int i = 0; i < nums.length; i++) {
+            int j = i;
 
-        int mid = len / 2;
-        int[] left = new int[mid];
-        int[] right = new int[len - mid];
-
-        for (int i = 0; i < mid; i++) {
-            left[i] = nums[i];
-        }
-        for (int i = mid; i < len; i++) {
-            right[i - mid] = nums[i];
-        }
-
-        sortColors(left);
-        sortColors(right);
-
-        // merge function
-        merge(nums, left, right);
-    }
-
-    public static void merge(int[] array, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
-        int ll = left.length;
-        int rr = right.length;
-
-        while (i < ll && j < rr) {
-            if (left[i] < right[j]) {
-                array[k] = left[i];
-                i++;
-            } else {
-                array[k] = right[j];
-                j++;
+            while (j > 0 && nums[j - 1] > nums[j]) {
+                int tmp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = tmp;
+                j--;
             }
-            k++;
         }
-        while (i < ll) {
-            array[k] = left[i];
-            i++;
-            k++;
-        }
-        while (j < rr) {
-            array[k] = right[j];
-            k++;
-            j++;
-        }
+     
     }
 }
