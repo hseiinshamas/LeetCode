60d47c8a1f344ba71f83cc0b5820d4b85582ec9e
Hussein Shamas <hssenshamas@gmail.com>
2022-07-18 15:58:17 +0300

    Time: 3 ms (97.78%), Space: 40.5 MB (99.35%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,33 +1,23 @@
 class Solution {
 
     public List<Integer> partitionLabels(String s) {
-        List<Integer> partitionLengths = new ArrayList<>();
-        int[] lastOccurrence = new int[26];
+        // get the last seen index for each letter 'a' - 'z'
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); ++i) last[s.charAt(i) - 'a'] = i;
 
-        //standard noting of lastOccurance of lowercase alph in a String.
-        for (int i = 0; i < s.length(); i++) {
-            lastOccurrence[s.charAt(i) - 'a'] = i;
-        }
-
-        //starting from first letter
-        int i = 0;
-        while (i < s.length()) {
-            //lastOccurrence of first letter
-            int end = lastOccurrence[s.charAt(i) - 'a'];
-            //edge case(start==end that means only 1 char)
-            if (i == end) {
-                partitionLengths.add(1);
-                i++;
-            } else {
-                //start j from next to i
-                int j = i + 1;
-                while (j != end) { //loop until we find lastindesxes of all chars in substring
-                    end = Math.max(end, lastOccurrence[s.charAt(j++) - 'a']);
-                }
-                partitionLengths.add(j - i + 1);
-                i = j + 1;
+        int boundary = 0, cnt = 0;
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < s.length(); ++i) {
+            // update boundary and cnt along the way
+            char curChar = s.charAt(i);
+            boundary = Math.max(boundary, last[curChar - 'a']);
+            cnt++;
+            // if we reach the boundary, we collect result and reset count to 0
+            if (i == boundary) {
+                ans.add(cnt);
+                cnt = 0;
             }
         }
-        return partitionLengths;
+        return ans;
     }
 }
