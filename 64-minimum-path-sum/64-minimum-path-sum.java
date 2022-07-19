40abcd3b94316feeef2ae4a9cd092b50e6468bc0
Hussein Shamas <hssenshamas@gmail.com>
2022-07-19 13:06:44 +0300

    Time: 2 ms (90.51%), Space: 45.7 MB (68.70%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,46 +1,35 @@
 class Solution {
 
-   
-      public static class Pair implements Comparable<Pair> {
-		int i;
-		int j;
-		int eff;
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
 
-		public Pair(int i, int j, int eff) {
-			this.i = i;
-			this.j = j;
-			this.eff = eff;
+        int m = grid.length;
+        int n = grid[0].length;
 
-		}
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
 
-		public int compareTo(Pair o) {
-			return this.eff - o.eff;
-		}
+        // initialize top row
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
 
-	}
+        // initialize left column
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
 
-	public int minPathSum(int[][] grid) {
-		PriorityQueue<Pair> q = new PriorityQueue<>();
-		q.add(new Pair(0, 0, grid[0][0]));
-		boolean[][] visited = new boolean[grid.length][grid[0].length];
-		int[][] dirs = { { 0, 1 }, { 1, 0 } };
-		while (!q.isEmpty()) {
-			Pair r = q.poll();
-			if (r.i == grid.length - 1 && r.j == grid[0].length - 1) {
-				return r.eff;
-			}
-			if (visited[r.i][r.j] == true)
-				continue;
-			visited[r.i][r.j] = true;
-			for (int k = 0; k < 2; k++) {
-				int i = r.i + dirs[k][0];
-				int j = r.j + dirs[k][1];
+        // fill up the dp table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
+            }
+        }
 
-				if (i >= 0 && j >= 0 && i <= grid.length - 1 && j <= grid[0].length - 1 && visited[i][j] == false) {
-					q.add(new Pair(i, j, grid[i][j] + r.eff));
-				}
-			}
-		}
-		return -1;
+        return dp[m - 1][n - 1];
     }
 }
