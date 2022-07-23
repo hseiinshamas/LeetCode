8fe76aa68ec99d6b040a3a3fa420c9fbf7813a47
Hussein Shamas <hssenshamas@gmail.com>
2022-07-23 10:33:02 +0300

    Time: 2 ms (65.33%), Space: 42.1 MB (82.25%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,18 +1,23 @@
 class Solution {
-    final char[][] L = { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' }, { 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
 
-    public List<String> letterCombinations(String D) {
-        int len = D.length();
-        List<String> ans = new ArrayList<>();
-        if (len == 0) return ans;
-        dfs(0, len, new StringBuilder(), ans, D);
-        return ans;
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return new ArrayList<>();
+
+        String[] dict = new String[] { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        List<String> combos = new ArrayList<>();
+        backtrack(combos, digits.toCharArray(), "", dict);
+        return combos;
     }
 
-    public void dfs(int pos, int len, StringBuilder sb, List<String> ans, String D) {
-        if (pos == len) ans.add(sb.toString()); else {
-            char[] letters = L[Character.getNumericValue(D.charAt(pos))];
-            for (int i = 0; i < letters.length; i++) dfs(pos + 1, len, new StringBuilder(sb).append(letters[i]), ans, D);
+    public void backtrack(List<String> combos, char[] digits, String s, String[] dict) {
+        if (s.length() == digits.length) {
+            combos.add(s);
+            return;
+        }
+        int i = s.length();
+        int digit = digits[i] - '0';
+        for (char letter : dict[digit].toCharArray()) {
+            backtrack(combos, digits, s + Character.toString(letter), dict);
         }
     }
 }
