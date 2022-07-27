5a2408a79a489fea073276e05f6d80187ec2fc95
Hussein Shamas <hssenshamas@gmail.com>
2022-07-27 10:45:13 +0300

    Time: 0 ms (100.00%), Space: 43.7 MB (5.13%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,15 +1,28 @@
 class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                set.add(matrix[i][j]);
-            }
-        }
+        if (matrix.length == 0) return false;
 
-        for (int numbers : set) {
-            if (set.contains(target)) return true;
+        return search(matrix, target);
+    }
+
+    private boolean search(int[][] matrix, int target) {
+        int row = matrix.length;
+        int columns = matrix[0].length;
+        int start = 0;
+        int end = row * columns - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int mid_element = matrix[mid / columns][mid % columns];
+
+            if (mid_element < target) {
+                start = mid + 1;
+            } else if (mid_element > target) {
+                end = mid - 1;
+            } else if (mid_element == target) {
+                return true;
+            }
         }
         return false;
     }
