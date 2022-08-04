a74da2088ca30cd82c62baba30658324c2c34a2e
Hussein Shamas <hssenshamas@gmail.com>
2022-08-04 09:54:12 +0300

    Time: 1 ms (99.89%), Space: 42.7 MB (94.59%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int jump(int[]A) {
+        int sc = 0;
+        int e = 0;
+        int max = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            max = Math.max(max, i + A[i]);
+            if (i == e) {
+                sc++;
+                e = max; // 2 
+            }
+        }
+        return sc;
+    }
+}
