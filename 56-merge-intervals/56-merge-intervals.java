336a2c8dcb9aa02bad44977d47b118fed0956373
Hussein Shamas <hssenshamas@gmail.com>
2022-08-10 10:40:27 +0300

    Time: 4 ms (99.80%), Space: 47.4 MB (89.32%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,20 +1,30 @@
 class Solution {
 
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) return intervals;
-
-        // sort intervals by starting value
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // if end of previous interval is more than the start of current interval then there is a overlap
-        LinkedList<int[]> mergedIntervals = new LinkedList<>();
-        for (int[] curr : intervals) {
-            //  if list empty or no overlap simply add current interval
-            if (mergedIntervals.isEmpty() || mergedIntervals.getLast()[1] < curr[0]) mergedIntervals.add(curr);
-            // else if overlap exists then merge curren tinterval with the previous interval
-            else mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1], curr[1]);
+        if(intervals == null || intervals.length == 0){
+            return new int[0][];
         }
-
-        return mergedIntervals.toArray(new int[0][]);
+		// seperate start and end 
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        List<int[]> res = new LinkedList<>();
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+		
+		// sort start[] end[] respectively
+        Arrays.sort(start);
+        Arrays.sort(end);
+		
+        int j = 0; // start time
+        for(int i = 0; i < intervals.length - 1; i++) { // iterate end time
+            if(start[i + 1] > end[i]) { // when collapse appears
+                res.add(new int[] {start[j], end[i]}); // record
+                j = i + 1; // move previous start time 
+            }    
+        }
+        res.add(new int[] {start[j], end[intervals.length - 1]}); // add last one
+        return res.toArray(new int[res.size()][]);
     }
 }
