dc99000523b7874ec1c28decef0011c2507e834d
Hussein Shamas <hssenshamas@gmail.com>
2022-08-14 10:37:53 +0300

    Time: 0 ms (100.00%), Space: 40.4 MB (98.47%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,25 +1,32 @@
 class Solution { // count sort
 
-    public void sortColors(int[] nums) {
-        int countZeroes = 0 ; 
-        int countOnes = 0; 
-        int countTwo = 0; 
-        for(int i = 0 ; i<nums.length; i++){
-            if(nums[i] == 0) countZeroes++; 
-            if(nums[i] == 1) countOnes++; 
-            if(nums[i] == 2) countTwo++; 
-        }
-        
-        for(int i = 0 ; i < countZeroes ; i++){
-            nums[i] = 0 ;
-        }
-        
-        for(int i = countZeroes ;  i < countZeroes + countOnes; i++){
-            nums[i] = 1; 
-        }
-        
-        for(int i = countZeroes+countOnes ; i < nums.length; i++){
-            nums[i] = 2; 
+    public void sortColors(int[] arr) {
+       int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        int tmp = 0;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0: {
+                    tmp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = tmp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                case 2: {
+                    tmp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = tmp;
+                    high--;
+                    break;
+                }
+            }
         }
     }
 }
+        
