87162f06b87bf5e52deb742c07e1f07ac9013983
Hussein Shamas <hssenshamas@gmail.com>
2022-08-20 12:09:42 +0300

    Time: 12 ms (74.72%), Space: 55.8 MB (13.53%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,30 +1,27 @@
 class Solution {
 
     public int[][] merge(int[][] intervals) {
-        if(intervals == null || intervals.length == 0){
-            return new int[0][];
+        List<int[]> res = new ArrayList<>();
+        
+        if(intervals.length == 0 || intervals == null) 
+            return res.toArray(new int[0][]);
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int[] i : intervals) {
+            if(i[0] <= end) {
+                end = Math.max(end, i[1]);
+            }
+            else {
+                res.add(new int[]{start, end});
+                start = i[0];
+                end = i[1];
+            }
         }
-		// seperate start and end 
-        int[] start = new int[intervals.length];
-        int[] end = new int[intervals.length];
-        List<int[]> res = new LinkedList<>();
-        for(int i = 0; i < intervals.length; i++){
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
-        }
-		
-		// sort start[] end[] respectively
-        Arrays.sort(start);
-        Arrays.sort(end);
-		
-        int j = 0; // start time
-        for(int i = 0; i < intervals.length - 1; i++) { // iterate end time
-            if(start[i + 1] > end[i]) { // when collapse appears
-                res.add(new int[] {start[j], end[i]}); // record
-                j = i + 1; // move previous start time 
-            }    
-        }
-        res.add(new int[] {start[j], end[intervals.length - 1]}); // add last one
-        return res.toArray(new int[res.size()][]);
+        res.add(new int[]{start, end});
+       return res.toArray(new int[0][]);
     }
 }
