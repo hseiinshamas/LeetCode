1bce122b7bd678f3c558477f9036e0740852cd25
Hussein Shamas <hssenshamas@gmail.com>
2022-08-30 05:26:07 +0300

    Time: 0 ms (100.00%), Space: 42.8 MB (57.35%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -10,7 +10,7 @@ class Solution {
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < (n / 2); j++) {
+            for (int j = 0; j < n/2; j++) {
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[i][n - 1 - j];
                 matrix[i][n - 1 - j] = tmp;
