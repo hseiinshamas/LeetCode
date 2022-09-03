af113289a480f0b94afe304c5aff577a64746639
Hussein Shamas <hssenshamas@gmail.com>
2022-09-03 11:16:06 +0300

    Time: 27 ms (56.94%), Space: 51.3 MB (30.38%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,19 +1,32 @@
 public class Solution {
-    int count = 0;
+    int total;
     
     public int findTargetSumWays(int[] nums, int S) {
-        calculate(nums, 0, 0, S);
-        return count;
+        total = Arrays.stream(nums).sum();
+        
+        int[][] memo = new int[nums.length][2 * total + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return calculate(nums, 0, 0, S, memo);
     }
     
-    public void calculate(int[] nums, int i, int sum, int S) {
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
         if (i == nums.length) {
             if (sum == S) {
-                count++;
+                return 1;
+            } else {
+                return 0;
             }
         } else {
-            calculate(nums, i + 1, sum + nums[i], S);
-            calculate(nums, i + 1, sum - nums[i], S);
+            if (memo[i][sum + total] != -1) {
+                return memo[i][sum + total];
+            }
+            
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+           
+            return memo[i][sum + total]= add + subtract;
         }
     }
 }
