41f9c21bb34316d9e0f9601138a77c78cd2d3356
Hussein Shamas <hssenshamas@gmail.com>
2022-09-14 09:29:42 +0300

    Time: 12 ms (90.94%), Space: 108 MB (50.88%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+        
+    public void preorder(TreeNode node, int path) {
+        if (node != null) {
+            // compute occurences of each digit 
+            // in the corresponding register
+            path = path ^ (1 << node.val);
+            // if it's a leaf check if the path is pseudo-palindromic
+            if (node.left == null && node.right == null) {
+                // check if at most one digit has an odd frequency
+                if ((path & (path - 1)) == 0) {
+                    ++count;
+                }
+            }
+            preorder(node.left, path);
+            preorder(node.right, path) ;
+        }
+    }
+
+    public int pseudoPalindromicPaths (TreeNode root) {
+        preorder(root, 0);
+        return count;
+    }
+}
