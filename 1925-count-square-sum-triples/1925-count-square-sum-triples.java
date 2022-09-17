80c6f3418106fc57c6a538b1f5d50adde2af5e4d
Hussein Shamas <hssenshamas@gmail.com>
2022-09-17 08:19:25 +0300

    Time: 82 ms (38.56%), Space: 40.7 MB (75.42%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -4,7 +4,7 @@ class Solution {
        for(int i = 1 ; i <= n ; i++){
            for(int j = i + 1 ; j <= n ;j++){
                for(int k = j + 1 ; k <= n ; k++){
-                   if(Math.pow(i,2) + Math.pow(j ,2) == Math.pow(k , 2)) count += 2;
+                   if(i * i  + j * j == k * k) count += 2;
                }
            }
        }
