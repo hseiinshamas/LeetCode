8bd94bec8826e5ea5efad9adefffe73dabc3f814
Hussein Shamas <hssenshamas@gmail.com>
2022-10-14 16:30:42 +0300

    Time: 9 ms (33.17%), Space: 219.4 MB (51.41%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,24 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+
+//     public ListNode deleteMiddle(ListNode head) {
+//         if (head.next == null) return null;
+//         int count = 0;
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (slow != null) {
+//             count++;
+//             slow = slow.next;
+//         }
+
+//         int mid = count / 2;
+//         for (int i = 0; i < mid - 1; i++) 
+//             fast = fast.next;
+
+//         fast.next = fast.next.next;
+//         return head;
+//     }
+// }
+
 class Solution {
-
     public ListNode deleteMiddle(ListNode head) {
-        if (head.next == null) return null;
-        int count = 0;
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (slow != null) {
-            count++;
+        // Edge case: return nullptr if there is only one node.
+        if (head.next == null)
+            return null;
+        
+        // Initialize two pointers, 'slow' and 'fast'.
+        ListNode slow = head, fast = head.next.next;
+        
+        // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
+        while (fast != null && fast.next != null) {
             slow = slow.next;
+            fast = fast.next.next;
         }
-
-        int mid = count / 2;
-        for (int i = 0; i < mid - 1; i++) 
-            fast = fast.next;
-
-        fast.next = fast.next.next;
+        
+        // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+        slow.next = slow.next.next;
         return head;
     }
 }
